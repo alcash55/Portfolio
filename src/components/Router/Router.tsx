7356d32a8768d968f22e05006557fb2c865e1e9c
Alex <alex.e.cash28@gmail.com>
2024-01-06 23:59:09 -0500

    added loading and error

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -8,8 +8,7 @@ import { Pages } from "../Pages";
 export const Router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Pages.Home />} />
-      <Route path="/404" element={<Pages.Error />} />
+      <Route path="/" element={<Pages.Home />} errorElement={<Pages.Error />} />
     </>
   )
 );
--- src/layout/Providers/index.tsx
+++ src/layout/Providers/index.tsx
@@ -2,12 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import { Router } from "../../components/Router/Router";
 import ThemeProvider from "../Theme";
 import AppShellProvider from "../../components/AppShell/AppShell";
+import { Loading } from "../../components/Loading/Loading";
 
 export function Providers() {
   return (
     <ThemeProvider>
       <AppShellProvider>
-        <RouterProvider router={Router} fallbackElement={<>loading</>} />
+        <RouterProvider router={Router} fallbackElement={<Loading />} />
       </AppShellProvider>
     </ThemeProvider>
   );
