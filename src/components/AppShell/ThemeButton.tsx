76a023009fe06dead6eb210372c575b7942f6655
Alex <alex.e.cash28@gmail.com>
2024-01-12 00:01:25 -0500

    added active button for themes

--- src/components/AppShell/ThemeButton.tsx
+++ src/components/AppShell/ThemeButton.tsx
@@ -2,17 +2,38 @@ import { Button, Stack } from "@mui/material";
 import { useColorMode } from "../../layout/Theme/Context";
 
 export const ThemeButton = () => {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, mode } = useColorMode();
 
   return (
     <Stack direction={"row"} spacing={1}>
-      <Button variant="outlined" onClick={() => toggleColorMode("red")}>
+      <Button
+        variant={
+          mode.palette?.background?.paper === "#731010"
+            ? "contained"
+            : "outlined"
+        }
+        onClick={() => toggleColorMode("red")}
+      >
         Red
       </Button>
-      <Button variant="outlined" onClick={() => toggleColorMode("dark")}>
+      <Button
+        variant={
+          mode.palette?.background?.paper === "#292929"
+            ? "contained"
+            : "outlined"
+        }
+        onClick={() => toggleColorMode("dark")}
+      >
         Dark
       </Button>
-      <Button variant="outlined" onClick={() => toggleColorMode("blue")}>
+      <Button
+        variant={
+          mode.palette?.background?.paper === "#24344d"
+            ? "contained"
+            : "outlined"
+        }
+        onClick={() => toggleColorMode("blue")}
+      >
         Blue
       </Button>
     </Stack>
