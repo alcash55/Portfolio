5b5a1ecff2eb1c412d3426f3434aa4de57c919dc
Alex <alex.e.cash28@gmail.com>
2024-01-23 03:55:16 -0500

    update sideNav layout

--- src/components/AppShell/InternalComponents/Layouts/SideNav.tsx
+++ src/components/AppShell/InternalComponents/Layouts/SideNav.tsx
@@ -1,40 +1,25 @@
 import { PropsWithChildren } from "react";
-import { Button, Fab, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  Fab,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+} from "@mui/material";
 import { Menu } from "@mui/icons-material";
 import { useSettingDrawer } from "../useSettingsDrawer";
 import { SettingsDrawer } from "../SettingsDrawer";
+import { Sidebar } from "../Sidebar";
 
 export const SideNav = ({ children }: PropsWithChildren) => {
   const { settingDrawer, setSettingDrawer } = useSettingDrawer();
-
-  const navItems = [
-    {
-      href: "#summary",
-      name: "Summary",
-    },
-    {
-      href: "#about",
-      name: "About",
-    },
-    {
-      href: "#projects",
-      name: "Projects",
-    },
-    {
-      href: "#contact",
-      name: "Contact",
-    },
-  ];
-
-  const SideNav = (
-    <Stack sx={{}}>
-      {navItems.map((item) => (
-        <Button href={item.href} key={item.name}>
-          {item.name}
-        </Button>
-      ))}
-    </Stack>
-  );
 
   return (
     <Stack px={2}>
@@ -42,21 +27,16 @@ export const SideNav = ({ children }: PropsWithChildren) => {
         settingDrawer={settingDrawer}
         setSettingDrawer={setSettingDrawer}
       />
-      <Stack>
-        <Stack direction={"row"}>
-          {SideNav}
-          {children}
-        </Stack>
-        <Fab
-          size="medium"
-          color="secondary"
-          aria-label="open settings drawer"
-          onClick={() => setSettingDrawer(true)}
-          sx={{ position: "fixed", bottom: 20, right: 20 }}
-        >
-          <Menu />
-        </Fab>
-      </Stack>
+      <Sidebar children={children} />
+      <Fab
+        size="medium"
+        color="secondary"
+        aria-label="open settings drawer"
+        onClick={() => setSettingDrawer(true)}
+        sx={{ position: "fixed", bottom: 20, right: 20 }}
+      >
+        <Menu />
+      </Fab>
     </Stack>
   );
 };
---
+++ src/components/AppShell/InternalComponents/Sidebar.tsx
@@ -0,0 +1,100 @@
+import { PropsWithChildren } from "react";
+import {
+  Drawer,
+  Box,
+  Button,
+  Toolbar,
+  Typography,
+  Divider,
+  Stack,
+} from "@mui/material";
+import {
+  Home,
+  ConnectWithoutContact,
+  Construction,
+  EmojiPeople,
+} from "@mui/icons-material";
+
+export const Sidebar = ({ children }: PropsWithChildren) => {
+  const SideBarTopItem = () => {
+    return (
+      <>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant={"h1"} fontSize={24} sx={{ color: "white" }}>
+            Alex Cash
+          </Typography>
+        </Toolbar>
+        <Divider sx={{}} />
+      </>
+    );
+  };
+
+  const SideBarItems = () => {
+    const navItems = [
+      {
+        href: "#summary",
+        name: "Summary",
+        icon: <Home />,
+      },
+      {
+        href: "#about",
+        name: "About",
+        icon: <EmojiPeople />,
+      },
+      {
+        href: "#projects",
+        name: "Projects",
+        icon: <Construction />,
+      },
+      {
+        href: "#contact",
+        name: "Contact",
+        icon: <ConnectWithoutContact />,
+      },
+    ];
+    return (
+      <Stack
+        spacing={2}
+        sx={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          py: 2,
+        }}
+      >
+        {navItems.map((item) => (
+          <Button
+            variant="contained"
+            startIcon={item.icon} // Render the icon component
+            href={item.href}
+            key={item.name}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </Stack>
+    );
+  };
+
+  return (
+    <Stack>
+      <Stack direction={"row"}>
+        <Box
+          component="nav"
+          position={"sticky"}
+          sx={{ width: 240, height: "100%", px: 1, py: 2, top: 0 }}
+        >
+          <SideBarTopItem />
+          <SideBarItems />
+        </Box>
+        <Box width={"100%"}>{children}</Box>
+      </Stack>
+    </Stack>
+  );
+};
