3b789fa9034b38128877602c8dffd9dab825530d
Alex <alex.e.cash28@gmail.com>
2024-01-22 22:21:45 -0500

    remove console.log and unused var

--- src/components/AppShell/InternalComponents/LayoutButton.tsx
+++ src/components/AppShell/InternalComponents/LayoutButton.tsx
@@ -2,35 +2,36 @@ import { Stack, Button } from "@mui/material";
 import { useAppShellLayout } from "../AppShell";
 
 export const LayoutButton = () => {
-  const { toggleLayout, layout } = useAppShellLayout();
-  console.log({ layout });
+  const { toggleLayout } = useAppShellLayout();
+
   return (
-    <Stack
-      direction={"row"}
-      spacing={1}
-      justifyContent={"center"}
-      alignItems={"center"}
-    >
-      <Button
-        variant={
-          localStorage.getItem("layout") === "default"
-            ? "contained"
-            : "outlined"
-        }
-        onClick={() => toggleLayout("default")}
+          <Stack
+        direction={"row"}
+        spacing={1}
+        justifyContent={"center"}
+        alignItems={"center"}
       >
-        Default
-      </Button>
-      <Button
-        variant={
-          localStorage.getItem("layout") === "sideNav"
-            ? "contained"
-            : "outlined"
-        }
-        onClick={() => toggleLayout("sideNav")}
-      >
-        Left Side Navigation
-      </Button>
-    </Stack>
+        <Button
+          variant={
+            localStorage.getItem("layout") === "default"
+              ? "contained"
+              : "outlined"
+          }
+          onClick={() => toggleLayout("default")}
+        >
+          Default
+        </Button>
+        <Button
+          variant={
+            localStorage.getItem("layout") === "sideNav"
+              ? "contained"
+              : "outlined"
+          }
+          onClick={() => toggleLayout("sideNav")}
+        >
+          Left Side Navigation
+        </Button>
+      </Stack>
+    </Card>
   );
 };
