33b6bc9cc02004befe82cfe3e073c2f51cc7d2de
Alex <alex.e.cash28@gmail.com>
2024-01-22 21:12:32 -0500

    add persisting theme

--- src/layout/Theme/Context.tsx
+++ src/layout/Theme/Context.tsx
@@ -1,5 +1,11 @@
 //need to add persisting theme on refresh
-import { createContext, PropsWithChildren, useContext, useState } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { ThemeProvider } from "@mui/material";
 import { redTheme } from "./redTheme";
 import { darkTheme } from "./darkTheme";
@@ -18,10 +24,17 @@ export const useColorMode = () => {
 export default function ToggleColorMode({ children }: PropsWithChildren) {
   const [mode, setMode] = useState(darkTheme);
 
+  useEffect(() => {
+    const initialTheme = localStorage.getItem("theme") || "dark";
+    toggleColorMode(initialTheme);
+  }, []); // Run only once on mount to set initial layout
+
   const toggleColorMode = (color: string) => {
     if (color === "red") setMode(redTheme);
     else if (color === "dark") setMode(darkTheme);
     else if (color === "blue") setMode(blueTheme);
+
+    localStorage.setItem("theme", color); //update local storage
   };
 
   const contextValue = {
