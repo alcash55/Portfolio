459ca4d0f48ba27892785fbb06fbc73b981e1924
Alex <alex.e.cash28@gmail.com>
2024-01-25 01:55:50 -0500

    add dist to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./dist",
+  },
   plugins: [react()],
   server: {
-    port:3000,
-    host: 'localhost',
+    port: 3000,
+    host: "localhost",
     open: true,
-  }
-})
+  },
+});
