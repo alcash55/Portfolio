99fd5e855b632d74d07f04adaa4f5d2bf399fe78
Alex <alex.e.cash28@gmail.com>
2024-01-25 02:38:02 -0500

    remove routes

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -8,8 +8,6 @@ import { Pages } from "../Pages";
 
 export const Router = createBrowserRouter(
   createRoutesFromElements(
-    <Routes>
-      <Route path="/" element={<Pages.Home />} errorElement={<Pages.Error />} />
-    </Routes>
+    <Route path="/" element={<Pages.Home />} errorElement={<Pages.Error />} />
   )
 );
