a7d1bc9ff236e8a2b381ac36edcb980a0f537996
Alex <alex.e.cash28@gmail.com>
2024-01-28 01:25:29 -0500

    update footer

--- src/components/AppShell/InternalComponents/Footer.tsx
+++ src/components/AppShell/InternalComponents/Footer.tsx
@@ -1,24 +1,35 @@
-import { Stack, Link, Toolbar } from "@mui/material";
+import { Stack, Link, Toolbar, Typography, Card } from "@mui/material";
 
 export const Footer = () => {
   return (
     <Toolbar component={"footer"}>
-      <Stack spacing={2}>
-        <Link
-          underline="hover"
-          target="_blank"
-          href="https://www.linkedin.com/in/alexander-cash/"
-        >
-          LinkedIn
-        </Link>
-        <Link
-          underline="hover"
-          target="_blank"
-          href="https://github.com/alcash55"
-        >
-          Github
-        </Link>
-      </Stack>
+      <Card sx={{ width: "100%", p: 2 }}>
+        <Stack spacing={2}>
+          <Typography>Sitemap</Typography>
+          <Link
+            underline="hover"
+            target="_blank"
+            href="https://www.linkedin.com/in/alexander-cash/"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            underline="hover"
+            target="_blank"
+            href="https://github.com/alcash55"
+          >
+            Github
+          </Link>
+          <Link
+            underline="hover"
+            target="_blank"
+            href="https://alcash55.github.io/Portfolio/sitemap.xml"
+          >
+            Sitemap.xml
+          </Link>
+          <Typography>All rights reserved © Alex Cash 2024</Typography>
+        </Stack>
+      </Card>
     </Toolbar>
   );
 };
