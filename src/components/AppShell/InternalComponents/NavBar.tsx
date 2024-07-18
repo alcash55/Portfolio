99905f986ae80c96726eb63c5be0c91322fdc62c
Alcash55 <alex.e.cash28@gmail.com>
2024-07-17 20:48:37 -0400

    add fade in for nav bar

--- src/components/AppShell/InternalComponents/NavBar.tsx
+++ src/components/AppShell/InternalComponents/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, AppBar, Toolbar, Button, IconButton } from "@mui/material";
+import { Box, AppBar, Toolbar, Button, IconButton, Fade } from "@mui/material";
 import { Menu } from "@mui/icons-material/";
 import { showNavBar } from "./showNavBar";
 
@@ -9,52 +9,54 @@ interface NavBarProps {
 export const NavBar = ({ setSettingDrawer }: NavBarProps) => {
   const isVisible = showNavBar();
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        top: 0,
-        position: "sticky",
-        zIndex: 1,
-        width: "100%",
-        transition: "top 1.0s",
-        visibility: isVisible ? "visible" : "hidden",
-      }}
-    >
-      <AppBar sx={{ bgcolor: "black" }}>
-        <Toolbar>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-evenly",
-              width: "100%",
-              flexWrap: "wrap",
-            }}
-          >
-            <Button variant="text" sx={{ color: "white" }} href="#summary">
-              Home
-            </Button>
-            <Button variant="text" sx={{ color: "white" }} href="#about">
-              About
-            </Button>
-            <Button variant="text" sx={{ color: "white" }} href="#experience">
-              Experience
-            </Button>
-            <Button variant="text" sx={{ color: "white" }} href="#projects">
-              Projects
-            </Button>
-            <Button variant="text" sx={{ color: "white" }} href="#contact">
-              Contact
-            </Button>
-          </Box>
-          <IconButton
-            aria-label="Open Settings Drawer"
-            onClick={() => setSettingDrawer(true)}
-          >
-            <Menu sx={{ color: "white" }} />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <Fade in={isVisible} timeout={1000}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          top: 0,
+          position: "sticky",
+          zIndex: 1,
+          width: "100%",
+          transition: "top 1.0s",
+          visibility: isVisible ? "visible" : "hidden",
+        }}
+      >
+        <AppBar sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                width: "100%",
+                flexWrap: "wrap",
+              }}
+            >
+              <Button variant="text" sx={{ color: "white" }} href="#summary">
+                Home
+              </Button>
+              <Button variant="text" sx={{ color: "white" }} href="#about">
+                About
+              </Button>
+              <Button variant="text" sx={{ color: "white" }} href="#experience">
+                Experience
+              </Button>
+              <Button variant="text" sx={{ color: "white" }} href="#projects">
+                Projects
+              </Button>
+              <Button variant="text" sx={{ color: "white" }} href="#contact">
+                Contact
+              </Button>
+            </Box>
+            <IconButton
+              aria-label="Open Settings Drawer"
+              onClick={() => setSettingDrawer(true)}
+            >
+              <Menu sx={{ color: "white" }} />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Fade>
   );
 };
