258bc68da81649f6fcad94c7bc25cf9dd4727778
Alcash55 <alex.e.cash28@gmail.com>
2024-07-20 17:40:22 -0400

    add inital workflow call for connect form

--- src/components/ConnectForm/useConnectForm.ts
+++ src/components/ConnectForm/useConnectForm.ts
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import useConnectNotification from "./useConnectNotification";
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+  auth: process.env.GH_TOKEN,
+});
 
 /**
  * Use Connect Form Hook that returns a set of functions to interact with the form
@@ -15,9 +20,28 @@ const useConnectForm = () => {
    * Todo: send message to discord using composite action
    * @see https://github.com/alcash55/ac-composite-actions/tree/main/notifications/discord-messages
    */
-  const sendMessage = () => {
-    // TODO: show notification on success and error
-    console.log({ name: name, email: email, message: message });
+  const sendMessage = async () => {
+    // TODO: Get workflow id from repo
+    try {
+      await octokit.rest.actions.createWorkflowDispatch({
+        owner: "alcash55",
+        repo: "ac-composite-actions",
+        workflow_id: "main.yml",
+        ref: "main",
+        inputs: {
+          message: `
+            ${name} has sent you a message
+            email: ${email}
+            message:${message}
+          `,
+        },
+      });
+
+      setOpen(true);
+    } catch (e) {
+      console.log(`Error: ${e}`);
+      setOpen(false);
+    }
   };
 
   return { setName, setEmail, setMessage, sendMessage, name, email, message };
