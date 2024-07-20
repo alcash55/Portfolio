b76fa72079a705aa6053d8701f2c8492b22b03fe
Alcash55 <alex.e.cash28@gmail.com>
2024-07-20 18:21:09 -0400

    cleanup default page

--- src/components/AppShell/InternalComponents/Layouts/Default.tsx
+++ src/components/AppShell/InternalComponents/Layouts/Default.tsx
@@ -1,14 +1,12 @@
 import { PropsWithChildren } from "react";
-import { Stack, Box, useTheme, useMediaQuery } from "@mui/material";
+import { Stack } from "@mui/material";
 import { Footer } from "../Footer";
 import { NavBar } from "../NavBar";
 import { SettingsDrawer } from "../SettingsDrawer";
 import { useSettingDrawer } from "../useSettingsDrawer";
 
 export const Default = ({ children }: PropsWithChildren) => {
-  const theme = useTheme();
   const { settingDrawer, setSettingDrawer } = useSettingDrawer();
-  const smallMobile = useMediaQuery(theme.breakpoints.down(335));
   return (
     <Stack>
       <NavBar setSettingDrawer={setSettingDrawer} />
