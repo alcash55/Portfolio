8b9448cc56e2f841293858ea10ca7297535df625
Alcash55 <alex.e.cash28@gmail.com>
2024-07-20 21:28:18 -0400

    Clean up error page

--- src/components/Pages/Error/Error.tsx
+++ src/components/Pages/Error/Error.tsx
@@ -1,9 +1,20 @@
-import { Stack } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 const Error = () => {
   return (
-    <Stack id="about" component={"section"}>
-      Error page
+    <Stack
+      id="about"
+      component={"section"}
+      width={"100%"}
+      height={"100vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      spacing={2}
+    >
+      <Typography variant="h1">404 :(</Typography>
+      <Button variant="contained" href="/Portfolio/">
+        Go Home
+      </Button>
     </Stack>
   );
 };
