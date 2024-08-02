39e6b3ca4da38110fc72ce4520cf5a39f4b2a052
Alex <alex.e.cash28@gmail.com>
2024-08-01 23:48:45 -0400

    add initial grid

--- src/components/Pages/Projects/Projects.tsx
+++ src/components/Pages/Projects/Projects.tsx
@@ -1,14 +1,22 @@
 import {
   Card,
+  CardActionArea,
   CardContent,
   CardHeader,
   CardMedia,
+  Grid,
   IconButton,
   Stack,
 } from "@mui/material";
 import LinkIcon from "@mui/icons-material/Link";
 
 const Projects = () => {
+  const projectsList = [
+    { name: "Project 1", img: "", href: "" },
+    { name: "Project 1", img: "", href: "" },
+    { name: "Project 1", img: "", href: "" },
+    { name: "Project 1", img: "", href: "" },
+  ];
   return (
     <Stack id="projects" component={"section"}>
       <Card
@@ -42,7 +50,30 @@ const Projects = () => {
           }
           title="Projects"
         />
-        <CardContent></CardContent>
+        <CardContent
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+          }}
+        >
+          <Grid container spacing={2} width={"100%"} justifyContent={"center"}>
+            {projectsList.map((project) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="100%"
+                      alt="logo"
+                      image={project.img}
+                    />
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </CardContent>
       </Card>
     </Stack>
   );
